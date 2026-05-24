d5e50943bec28565bb7d1bf99a4aa6b4b223e8f4
Andrzej Pawlak <s33338@pjwstk.edu.pl>
2026-05-24 12:17:06 +0200

    Zmiany

---
+++ APBD_cw12_git_s33338/APBD_cw12_git_s33338/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APBD_cw12_git_s33338.Models;
+
+public partial class Room
+{
+    public string Id { get; set; } = null!;
+
+    public int WardId { get; set; }
+
+    public bool HasTv { get; set; }
+
+    public virtual ICollection<Bed> Beds { get; set; } = new List<Bed>();
+
+    public virtual Ward Ward { get; set; } = null!;
+}
